b27d20e0840d70ab60b4fc5ecabafefc527cf3f5
erin <nire9221@gmail.com>
2020-04-14 14:30:04 -0700

    assign1

---
+++ src/itc155/Assignment1.java
@@ -0,0 +1,24 @@
+package itc155;
+
+import java.awt.*;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+
+		DrawingPanel panel = new DrawingPanel(220,150);
+		panel.setBackground(Color.YELLOW);
+		
+		Graphics g = panel.getGraphics();
+
+		g.setColor(Color.BLUE);
+		g.fillOval(50,25,40,40);
+		g.fillOval(130,25,40,40);
+		
+		g.setColor(Color.RED);
+		g.fillRect(70,45,80,80);
+		g.setColor(Color.BLACK);
+		g.drawLine(70,85,150,85);
+	}
+
+}
